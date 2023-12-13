bbe74f17b15f32c201d0ccfaf223803da3d5e0fa
Leom <jch.lion97@gmail.com>
2023-12-12 20:37:36 -0500

    redux brach

--- client/src/store/slices/products/thunks.js
+++ client/src/store/slices/products/thunks.js
@@ -1,7 +1,18 @@
-import { handleShopList, itemsAdded, setCurrentPage } from "./productSlice";
+import { handleShopList, itemsAdded, setCurrentPage, setProducts, startLoading } from "./productSlice";
 
 
 
+
+export const getProducts = (products, page = 1) => {
+  return async (dispatch) => {
+    dispatch(startLoading());
+
+    
+
+    dispatch(setProducts(products));
+    dispatch(setCurrentPage(page));
+  };
+};
 export const openShopList = (value) => {
   return async (dispatch) => {
     dispatch(handleShopList(value));
