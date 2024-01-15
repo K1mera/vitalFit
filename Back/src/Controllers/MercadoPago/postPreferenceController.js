776a18f5d5c9849f02430195af06d11d1a114d6a
MDeFaria <maria.defariasf@gmail.com>
2024-01-15 17:41:05 -0400

    Agregados parámetros para el posterior acceso a la orden de compra

--- Back/src/Controllers/MercadoPago/postPreferenceController.js
+++ Back/src/Controllers/MercadoPago/postPreferenceController.js
@@ -1,31 +1,31 @@
-require("dotenv").config()
+require("dotenv").config();
 const { ACCESS_TOKEN } = process.env;
 
-const { MercadoPagoConfig, Preference } = require('mercadopago');
+const { MercadoPagoConfig, Preference } = require("mercadopago");
 const client = new MercadoPagoConfig({ accessToken: ACCESS_TOKEN });
 
-const postPreference = async ({ title, unit_price, quantity }) => {
-        const body = {
-            items: [
-                {
-                    title,
-                    unit_price: Number(unit_price),
-                    quantity: Number(quantity)
-                }
-            ],
-            back_urls: {
-                success: "http://localhost:5173/home",
-                failure : "http://localhost:5173/home",
-                pending : "http://localhost:5173/home"
-            },
-            auto_return : "approved"
+const postPreference = async ({ userId, userEmail, items, orderId }) => {
+  const body = {
+    items: /* [
+      {
+        title,
+        unit_price: Number(unit_price),
+        quantity: Number(quantity),
+      },
+    ], */ items,
+    back_urls: {
+      success: "http://localhost:5173/checkout/successfull",
+      failure: "http://localhost:5173/home",
+      pending: "http://localhost:5173/home",
+    },
+    external_reference: orderId + "-_" + userId + "-_" + userEmail,
+    auto_return: "approved",
+  };
 
-        }
-        
-        const preference = new Preference(client)
-        const result = await preference.create({ body })
+  const preference = new Preference(client);
+  const result = await preference.create({ body });
 
-        return {id: result.id}
-}
+  return { id: result.id };
+};
 
-module.exports = postPreference
+module.exports = postPreference;
