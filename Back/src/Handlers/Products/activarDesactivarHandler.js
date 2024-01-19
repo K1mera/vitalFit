fe9b02afc794374979781cb76763b73c65386f78
MDeFaria <maria.defariasf@gmail.com>
2024-01-19 12:45:05 -0400

    activar y desactivar productos

--- Back/src/Handlers/Products/activarDesactivarHandler.js
+++ Back/src/Handlers/Products/activarDesactivarHandler.js
@@ -2,17 +2,13 @@ const activarDesactivarProducto = require("../../Controllers/Products/activarDes
 
 const activarDesactivarHandler = async (req, res) => {
   const { id } = req.params;
-  const { active } = req.body;
 
   try {
-    await activarDesactivarProducto(id, active);
-    if (active == true)
-      return res
-        .status(200)
-        .json({ message: "El producto ha sido activado con éxito" });
+    await activarDesactivarProducto(id);
+
     return res
       .status(200)
-      .json({ message: "El producto ha sido desactivado con éxito" });
+      .json({ message: "El producto ha sido modificado con éxito" });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
