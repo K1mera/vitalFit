c74a56cd3298adfc49b268bbc06e253f65e6451d
DeFariaM <129224622+DeFariaM@users.noreply.github.com>
2024-01-19 12:58:01 -0400

    Update productsInstance.js

--- client/src/api/productsInstance.js
+++ client/src/api/productsInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const productsIns = axios.create({
-  //baseURL: "https://vitalfitapi.onrender.com",
-  baseURL: "http://localhost:3001",
+  baseURL: "https://vitalfitapi.onrender.com",
+  //baseURL: "http://localhost:3001",
 });
 // https://vitalfitapi.onrender.com
